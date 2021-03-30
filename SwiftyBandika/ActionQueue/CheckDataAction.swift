bcbef5baefa7c2feb6fd6653f23005cfc685cab4
mr-elbe5 <miro@elbe5.de>
2021-03-30 10:15:45 +0200

    html localization with request language

--- SwiftyBandika/ActionQueue/CheckDataAction.swift
+++ SwiftyBandika/ActionQueue/CheckDataAction.swift
@@ -24,7 +24,6 @@ class CheckDataAction : QueuedAction{
     }
 
     override func execute() {
-        print("checking data")
         IdService.instance.checkIdChanged()
         UserContainer.instance.checkChanged()
         ContentContainer.instance.checkChanged()
--- SwiftyBandika/App/AppDelegate.swift
+++ SwiftyBandika/App/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         Paths.initPaths()
+        Localizer.initialize(languages: ["en","de"])
         initializeData()
         NSApplication.shared.mainMenu = MainMenu()
         mainWindowController.showWindow(nil)
